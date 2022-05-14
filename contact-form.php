85dcde5722e8f25667dcb19d5f0fdde586e0e6a0
Lucas <lucasdelvillar14@gmail.com>
2022-05-13 20:04:13 -0700

    Add updated portfolio files

---
+++ contact-form.php
@@ -0,0 +1,32 @@
+<?php
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['message'];
+
+$mail_to = 'lucasdelvillar14+dev@gmail.com';
+$subject = 'Message from a site visitor '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to lucasdelvillar14+dev@gmail.com');
+		window.location = 'index.html';
+	</script>
+<?php
+}
+?>
